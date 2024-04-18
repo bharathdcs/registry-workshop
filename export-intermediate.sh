78817fb1550ccd59f96c04fabb97fcf52bb74897
Bharath <bharathdcs@gmail.com>
2024-04-18 17:52:09 +1000

    Update export-intermediate.sh

--- export-intermediate.sh
+++ export-intermediate.sh
@@ -17,24 +17,3 @@ sleep 10
 mkdir ~/green
 tar -xzvf ~/cpd-cli-workspace.tar.gz -C ~/green
 
-export CPD_CLI_MANAGE_WORKSPACE=~/green/cpd-cli-workspace
-
-echo "Logging into the private registry"
-sleep 20
-cpd-cli manage login-private-registry \
-${PRIVATE_REGISTRY_LOCATION} \
-${PRIVATE_REGISTRY_PUSH_USER} \
-${PRIVATE_REGISTRY_PUSH_PASSWORD}
-
-
-echo "Mirror the images to private registry"
-sleep 20
-cpd-cli manage mirror-images \
---components=${COMPONENTS} \
---release=${VERSION} \
---source_registry=127.0.0.1:12443 \
---target_registry=${PRIVATE_REGISTRY_LOCATION} \
---arch=${IMAGE_ARCH} \
---case_download=false
-
-
