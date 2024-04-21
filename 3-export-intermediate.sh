f482850aca1182caa17496f23231d27be8097eeb
Bharath <bharathdcs@gmail.com>
2024-04-21 16:27:13 +1000

    Update 3-export-intermediate.sh

--- 3-export-intermediate.sh
+++ 3-export-intermediate.sh
@@ -1,5 +1,5 @@
 echo "Exporting the workspace"
-cd ~/cpd-cli-linux-EE-13.1.3-98
+cd ~/cpd-cli-linux-EE-13.1.4-109
 
 tar -czvf ~/cpd-cli-workspace.tar.gz cpd-cli-workspace
 
@@ -8,9 +8,9 @@ sleep 10
 
 podman rmi icr.io/cpopen/cpd/olm-utils-v2:latest --force
 
-export PATH=~/cpd-cli-linux-EE-13.1.3-98/:$PATH
+export PATH=~/cpd-cli-linux-EE-13.1.4-109/:$PATH
 
-rm -rf ~/cpd-cli-linux-EE-13.1.3-98/cpd-cli-workspace
+rm -rf ~/cpd-cli-linux-EE-13.1.4-109/cpd-cli-workspace
 
 echo "Lets simulate moving the workspace to a green zone" 
 echo ".................."
