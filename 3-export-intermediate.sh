5693e09b45315391576019aaf633b3d81ee53ddd
Bharath <bharathdcs@gmail.com>
2024-04-21 17:00:48 +1000

    Update 3-export-intermediate.sh

--- 3-export-intermediate.sh
+++ 3-export-intermediate.sh
@@ -1,5 +1,7 @@
 echo "Exporting the workspace"
 cd ~/cpd-cli-linux-EE-13.1.4-109
+mkdir ~/green
+export CPD_CLI_MANAGE_WORKSPACE=~/green/cpd-cli-workspace
 
 tar -czvf ~/cpd-cli-workspace.tar.gz cpd-cli-workspace
 
@@ -15,7 +17,7 @@ rm -rf ~/cpd-cli-linux-EE-13.1.4-109/cpd-cli-workspace
 echo "Lets simulate moving the workspace to a green zone" 
 echo ".................."
 sleep 10
+cpd-cli manage test
 
-mkdir ~/green
 tar -xzvf ~/cpd-cli-workspace.tar.gz -C ~/green
 
