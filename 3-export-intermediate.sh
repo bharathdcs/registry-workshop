0167e4c59a53abdea6493858d7a2501899e00ab9
Bharath <bharathdcs@gmail.com>
2024-04-21 17:27:22 +1000

    Update 3-export-intermediate.sh

--- 3-export-intermediate.sh
+++ 3-export-intermediate.sh
@@ -1,9 +1,8 @@
 echo "Exporting the workspace"
-cd ~/cpd-cli-linux-EE-13.1.4-109
 mkdir ~/green
-export CPD_CLI_MANAGE_WORKSPACE=~/green/cpd-cli-workspace
+cd ~/cpd-cli-linux-EE-13.1.4-109
 
-tar -czvf ~/cpd-cli-workspace.tar.gz ~/cpd-cli-linux-EE-13.1.4-109/cpd-cli-workspace
+tar -czvf ~/cpd-cli-workspace.tar.gz cpd-cli-workspace
 
 echo "stop and remove the olm-utils image"
 sleep 10
@@ -12,12 +11,14 @@ podman rmi icr.io/cpopen/cpd/olm-utils-v2:latest --force
 
 export PATH=~/cpd-cli-linux-EE-13.1.4-109/:$PATH
 
-rm -rf ~/cpd-cli-linux-EE-13.1.4-109/cpd-cli-workspace
+#rm -rf ~/cpd-cli-linux-EE-13.1.4-109/cpd-cli-workspace
 
 echo "Lets simulate moving the workspace to a green zone" 
 echo ".................."
 sleep 10
-cpd-cli manage test
+export CPD_CLI_MANAGE_WORKSPACE=~/green/cpd-cli-workspace
+
+cpd-cli manage restart-container
 
 tar -xzvf ~/cpd-cli-workspace.tar.gz -C ~/green
 
